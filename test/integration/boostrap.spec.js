c9ab291242cb23985ea6f3618a5b51c2faf081e8
lykmapipo <lallyelias87@gmail.com>
2019-05-28 23:11:20 +0300

    chore(deps): force latest versions

--- test/integration/boostrap.spec.js
+++ test/integration/boostrap.spec.js
@@ -1,38 +1,20 @@
 'use strict';
 
-
 /* dependencies */
-const async = require('async');
-const kue = require('kue');
 const { connect, clear, drop } = require('@lykmapipo/mongoose-test-helpers');
+const { worker } = require('mongoose-kue');
 
-/* clear redis database */
-const clean = done => {
-  const redis = kue.redis.createClientFactory({
-    redis: {}
-  });
-  redis.keys('q*', (error, rows) => {
-    if (error) {
-      done(error);
-    } else {
-      async.each(rows, (row, next) => {
-        redis.del(row, next);
-      }, done);
-    }
-  });
-};
+/* clear queue test database */
+before(done => worker.clear(done));
 
-/* clear redis */
-before(clean);
+/* setup mongo test database */
+before(done => connect(done));
 
-/* setup database */
-before((done) => connect(done));
+/* clear mongo test database */
+before(done => clear(done));
 
-/* clear database */
-before((done) => clear(done));
+/* drop mongo test database */
+after(done => drop(done));
 
-/* drop database */
-after((done) => drop(done));
-
-/* clear redis */
-after(clean);
+/* clear queue test database */
+after(done => worker.clear(done));
