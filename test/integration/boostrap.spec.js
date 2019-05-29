e6ba43c1ad328704a762667891fd9ae674a62a9e
lykmapipo <lallyelias87@gmail.com>
2019-05-29 18:19:06 +0300

    test: refactor queue specs to work

--- test/integration/boostrap.spec.js
+++ test/integration/boostrap.spec.js
@@ -4,17 +4,20 @@
 const { connect, clear, drop } = require('@lykmapipo/mongoose-test-helpers');
 const { worker } = require('mongoose-kue');
 
-/* clear queue test database */
-before(done => worker.clear(done));
-
 /* setup mongo test database */
 before(done => connect(done));
+
+/* clear queue test database */
+before(done => worker.clear(done));
 
 /* clear mongo test database */
 before(done => clear(done));
 
-/* drop mongo test database */
-after(done => drop(done));
-
 /* clear queue test database */
 after(done => worker.clear(done));
+
+/* shutdown queue */
+after(done => worker.stop(done));
+
+/* drop mongo test database */
+after(done => drop(done));
--- test/integration/message.queue.tz.ega.spec.js
+++ test/integration/message.queue.tz.ega.spec.js
@@ -1,69 +1,58 @@
 'use strict';
 
-
 /* dependencies */
-const path = require('path');
-const { expect } = require('chai');
-const { clear } = require('@lykmapipo/mongoose-test-helpers');
-const { worker } = require('mongoose-kue');
-const { Message } = require(path.join(__dirname, '..', '..'));
+const { expect, clear } = require('@lykmapipo/mongoose-test-helpers');
+const { Message, worker } = require('../..');
 
-
-describe.skip('tz ega transport queue', () => {
+describe('tz ega transport queue', () => {
 
   before(() => {
     process.env.DEBUG = true;
   });
 
   before(done => clear(done));
-
+  before(done => worker.clear(done));
   before(done => worker.reset(done));
-
   before(() => worker.start({ types: Message.TYPES }));
 
-
-  it('should be able to queue message', (done) => {
+  it('should be able to queue message', done => {
 
     const message =
-      Message.fakeExcept('sentAt', 'failedAt', 'deliveredAt');
+      Message.fakeExcept('sentAt', 'failedAt', 'deliveredAt', 'mode');
     message.transport = 'tz-ega-sms';
 
-    //listen to queue events
-    worker.queue.on('job complete', function (id, result) {
-        expect(id).to.exist;
-        expect(result).to.exist;
-        expect(result.to).to.exist;
-        expect(result.to).to.be.eql(message.to);
-        expect(result._id).to.exist;
-        expect(result.transport).to.be.equal('tz-ega-sms');
-        expect(result.sentAt).to.exist;
-        expect(result.deliveredAt).to.exist;
-        expect(result.failedAt).to.not.exist;
-        expect(result.result).to.exist;
-        expect(result.result.success).to.exist;
-        expect(result.result.success).to.be.true;
-        done();
-      })
-      .on('job queued', function (queued) {
-        expect(queued._id).to.be.eql(message._id);
-      })
-      .on('job failed', function (error) {
-        done(new Error(error));
-      })
-      .on('job error', function (error) {
-        done(error);
-      });
+    // listen to queue events
+    worker.queue.on('job complete', (id, result) => {
+      expect(id).to.exist;
+      expect(result).to.exist;
+      expect(result.to).to.exist;
+      expect(result.to).to.be.eql(message.to);
+      expect(result._id).to.exist;
+      expect(result.transport).to.be.equal('tz-ega-sms');
+      expect(result.sentAt).to.exist;
+      expect(result.deliveredAt).to.exist;
+      expect(result.failedAt).to.not.exist;
+      expect(result.result).to.exist;
+      expect(result.result.success).to.exist;
+      expect(result.result.success).to.be.true;
+    }).on('job remove', (jobId, jobType) => {
+      expect(jobId).to.exist;
+      expect(jobType).to.exist;
+      done();
+    }).on('job queued', queued => {
+      expect(queued._id).to.be.eql(message._id);
+    }).on('error', error => {
+      done(error);
+    });
 
     message.queue();
-
   });
 
+  after(done => worker.clear(done));
   after(done => worker.stop(done));
-
   after(done => clear(done));
 
   after(() => {
     delete process.env.DEBUG;
   });
-
 });
