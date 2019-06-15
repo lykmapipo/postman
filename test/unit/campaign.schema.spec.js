fbbda9670c3131836ccfef5a4870fa6d35c7d808
lykmapipo <lallyelias87@gmail.com>
2019-06-15 18:15:59 +0300

    feat: add form, subject and message on campaign

--- test/unit/campaign.schema.spec.js
+++ test/unit/campaign.schema.spec.js
@@ -3,9 +3,57 @@
 
 /* dependencies */
 const path = require('path');
+const { SchemaTypes } = require('@lykmapipo/mongoose-common');
 const { expect } = require('@lykmapipo/mongoose-test-helpers');
 const { Campaign } = require(path.join(__dirname, '..', '..'));
 
 describe('Campaign Schema', () => {
-  expect(Campaign).to.exist;
+  it('should have form field', () => {
+    const form = Campaign.path('form');
+
+    expect(form).to.exist;
+    expect(form).to.be.an.instanceof(SchemaTypes.String);
+    expect(form.instance).to.be.equal('String');
+    expect(form).to.be.an('object');
+    expect(form.options.type).to.be.a('function');
+    expect(form.options.type.name).to.be.equal('String');
+    expect(form.options.enum).to.exist;
+    expect(form.options.enum).to.be.eql(Campaign.FORMS);
+    expect(form.options.trim).to.be.true;
+    expect(form.options.default).to.exist;
+    expect(form.options.index).to.be.true;
+    expect(form.options.searchable).to.be.true;
+    expect(form.options.taggable).to.be.true;
+    expect(form.options.fake).to.exist;
+  });
+
+  it('should have subject field', function () {
+    const subject = Campaign.path('subject');
+
+    expect(subject).to.exist;
+    expect(subject).to.be.an.instanceof(SchemaTypes.String);
+    expect(subject.instance).to.be.equal('String');
+    expect(subject).to.be.an('object');
+    expect(subject.options.type).to.be.a('function');
+    expect(subject.options.type.name).to.be.equal('String');
+    expect(subject.options.trim).to.be.true;
+    expect(subject.options.index).to.be.true;
+    expect(subject.options.searchable).to.be.true;
+    expect(subject.options.fake).to.exist;
+  });
+
+  it('should have message field', function () {
+    const message = Campaign.path('message');
+
+    expect(message).to.exist;
+    expect(message).to.be.an.instanceof(SchemaTypes.String);
+    expect(message.instance).to.be.equal('String');
+    expect(message).to.be.an('object');
+    expect(message.options.type).to.be.a('function');
+    expect(message.options.type.name).to.be.equal('String');
+    expect(message.options.trim).to.be.true;
+    expect(message.options.index).to.be.true;
+    expect(message.options.searchable).to.be.true;
+    expect(message.options.fake).to.exist;
+  });
 });
