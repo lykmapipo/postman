8ed527b9b8f1c99050d3abaed4b3d07659cf6b31
lykmapipo <lallyelias87@gmail.com>
2019-06-20 02:44:56 +0300

    feat: implement campaign send

--- test/integration/campaign.send.spec.js
+++ test/integration/campaign.send.spec.js
@@ -1,10 +1,21 @@
 'use strict';
 
 /* dependencies */
+const _ = require('lodash');
 const { faker, clear, expect } = require('@lykmapipo/mongoose-test-helpers');
 const { Campaign } = require('../..');
 
-describe.only('campaign send', () => {
+const fakeContacts = (limit = 1) => {
+  const contacts = _.map(_.range(limit), () => {
+    const name = faker.name.findName();
+    const email = faker.internet.email();
+    const mobile = faker.helpers.replaceSymbolWithNumber('255714######');
+    return { name, email, mobile };
+  });
+  return limit === 1 ? _.first(contacts) : contacts;
+};
+
+describe('campaign send', () => {
   before(() => {
     process.env.DEBUG = true;
   });
@@ -12,10 +23,7 @@ describe.only('campaign send', () => {
   before(done => clear(done));
 
   it('should be able to send without fetchContacts', done => {
-    const name = faker.name.findName();
-    const email = faker.internet.email();
-    const mobile = faker.helpers.replaceSymbolWithNumber('255714######');
-    const to = { name, email, mobile };
+    const to = fakeContacts();
 
     const campaign = Campaign.fake();
     campaign.to = [to];
@@ -25,6 +33,24 @@ describe.only('campaign send', () => {
       expect(error).to.not.exist;
       expect(sent).to.exist;
       expect(messages).to.exist.and.have.length(1);
+      done(error, sent);
+    });
+  });
+
+  it('should be able to send with fetchContacts', done => {
+    const to = fakeContacts(4);
+
+    Campaign.fetchContacts = (criteria, done) => {
+      return done(null, to);
+    };
+
+    const campaign = Campaign.fake();
+    expect(campaign.send).to.exist;
+
+    campaign.send((error, sent, messages) => {
+      expect(error).to.not.exist;
+      expect(sent).to.exist;
+      expect(messages).to.exist.and.have.length(5);
       done(error, sent);
     });
   });
