06b71d24aeab4f6fd24d542773a8e761a58ef26b
lykmapipo <lallyelias87@gmail.com>
2019-06-20 00:57:22 +0300

    feat: add campaign to message

--- test/unit/message.schema.spec.js
+++ test/unit/message.schema.spec.js
@@ -1,18 +1,30 @@
 'use strict';
 
 /* dependencies */
-const path = require('path');
-const { expect } = require('chai');
-const mongoose = require('mongoose');
-const Types = mongoose.Schema.Types;
-const { Message } = require(path.join(__dirname, '..', '..'));
+const { expect } = require('@lykmapipo/mongoose-test-helpers');
+const { SchemaTypes } = require('@lykmapipo/mongoose-common');
+const { Message, Campaign } = require('../..');
 
 describe('Message Schema', () => {
+  it('should have campaign field', () => {
+    const campaign = Message.path('campaign');
+
+    expect(campaign).to.exist;
+    expect(campaign).to.be.instanceof(SchemaTypes.ObjectId);
+    expect(campaign.options).to.exist;
+    expect(campaign.options).to.be.an('object');
+    expect(campaign.options.type).to.exist;
+    expect(campaign.options.ref).to.exist;
+    expect(campaign.options.ref).to.be.equal(Campaign.MODEL_NAME);
+    expect(campaign.options.default).to.be.undefined;
+    expect(campaign.options.index).to.be.true;
+  });
+
   it('should have type field', function() {
     const type = Message.path('type');
 
     expect(type).to.exist;
-    expect(type).to.be.an.instanceof(Types.String);
+    expect(type).to.be.an.instanceof(SchemaTypes.String);
     expect(type.instance).to.be.equal('String');
     expect(type).to.be.an('object');
     expect(type.options.type).to.be.a('function');
@@ -30,7 +42,7 @@ describe('Message Schema', () => {
     const mime = Message.path('mime');
 
     expect(mime).to.exist;
-    expect(mime).to.be.an.instanceof(Types.String);
+    expect(mime).to.be.an.instanceof(SchemaTypes.String);
     expect(mime.instance).to.be.equal('String');
     expect(mime).to.be.an('object');
     expect(mime.options.type).to.be.a('function');
@@ -48,7 +60,7 @@ describe('Message Schema', () => {
     const direction = Message.path('direction');
 
     expect(direction).to.exist;
-    expect(direction).to.be.an.instanceof(Types.String);
+    expect(direction).to.be.an.instanceof(SchemaTypes.String);
     expect(direction.instance).to.be.equal('String');
     expect(direction).to.be.an('object');
     expect(direction.options.type).to.be.a('function');
@@ -66,7 +78,7 @@ describe('Message Schema', () => {
     const state = Message.path('state');
 
     expect(state).to.exist;
-    expect(state).to.be.an.instanceof(Types.String);
+    expect(state).to.be.an.instanceof(SchemaTypes.String);
     expect(state.instance).to.be.equal('String');
     expect(state).to.be.an('object');
     expect(state.options.type).to.be.a('function');
@@ -84,7 +96,7 @@ describe('Message Schema', () => {
     const mode = Message.path('mode');
 
     expect(mode).to.exist;
-    expect(mode).to.be.an.instanceof(Types.String);
+    expect(mode).to.be.an.instanceof(SchemaTypes.String);
     expect(mode.instance).to.be.equal('String');
     expect(mode).to.be.an('object');
     expect(mode.options.type).to.be.a('function');
@@ -102,7 +114,7 @@ describe('Message Schema', () => {
     const bulk = Message.path('bulk');
 
     expect(bulk).to.exist;
-    expect(bulk).to.be.an.instanceof(Types.String);
+    expect(bulk).to.be.an.instanceof(SchemaTypes.String);
     expect(bulk.instance).to.be.equal('String');
     expect(bulk).to.be.an('object');
     expect(bulk.options.type).to.be.a('function');
@@ -117,7 +129,7 @@ describe('Message Schema', () => {
     const sender = Message.path('sender');
 
     expect(sender).to.exist;
-    expect(sender).to.be.an.instanceof(Types.String);
+    expect(sender).to.be.an.instanceof(SchemaTypes.String);
     expect(sender.instance).to.be.equal('String');
     expect(sender).to.be.an('object');
     expect(sender.options.type).to.be.a('function');
@@ -132,7 +144,7 @@ describe('Message Schema', () => {
     const to = Message.path('to');
 
     expect(to).to.exist;
-    expect(to).to.be.an.instanceof(Types.Array);
+    expect(to).to.be.an.instanceof(SchemaTypes.Array);
     expect(to.instance).to.be.equal('Array');
     expect(to).to.be.an('object');
     expect(to.options.type[0]).to.be.a('function');
@@ -147,7 +159,7 @@ describe('Message Schema', () => {
     const cc = Message.path('cc');
 
     expect(cc).to.exist;
-    expect(cc).to.be.an.instanceof(Types.Array);
+    expect(cc).to.be.an.instanceof(SchemaTypes.Array);
     expect(cc.instance).to.be.equal('Array');
     expect(cc).to.be.an('object');
     expect(cc.options.type[0]).to.be.a('function');
@@ -161,7 +173,7 @@ describe('Message Schema', () => {
     const bcc = Message.path('bcc');
 
     expect(bcc).to.exist;
-    expect(bcc).to.be.an.instanceof(Types.Array);
+    expect(bcc).to.be.an.instanceof(SchemaTypes.Array);
     expect(bcc.instance).to.be.equal('Array');
     expect(bcc).to.be.an('object');
     expect(bcc.options.type[0]).to.be.a('function');
@@ -175,7 +187,7 @@ describe('Message Schema', () => {
     const subject = Message.path('subject');
 
     expect(subject).to.exist;
-    expect(subject).to.be.an.instanceof(Types.String);
+    expect(subject).to.be.an.instanceof(SchemaTypes.String);
     expect(subject.instance).to.be.equal('String');
     expect(subject).to.be.an('object');
     expect(subject.options.type).to.be.a('function');
@@ -190,7 +202,7 @@ describe('Message Schema', () => {
     const body = Message.path('body');
 
     expect(body).to.exist;
-    expect(body).to.be.an.instanceof(Types.String);
+    expect(body).to.be.an.instanceof(SchemaTypes.String);
     expect(body.instance).to.be.equal('String');
     expect(body).to.be.an('object');
     expect(body.options.type).to.be.a('function');
@@ -205,7 +217,7 @@ describe('Message Schema', () => {
     const queuedAt = Message.path('queuedAt');
 
     expect(queuedAt).to.exist;
-    expect(queuedAt).to.be.an.instanceof(Types.Date);
+    expect(queuedAt).to.be.an.instanceof(SchemaTypes.Date);
     expect(queuedAt.instance).to.be.equal('Date');
     expect(queuedAt).to.be.an('object');
     expect(queuedAt.options.type).to.be.a('function');
@@ -218,7 +230,7 @@ describe('Message Schema', () => {
     const sentAt = Message.path('sentAt');
 
     expect(sentAt).to.exist;
-    expect(sentAt).to.be.an.instanceof(Types.Date);
+    expect(sentAt).to.be.an.instanceof(SchemaTypes.Date);
     expect(sentAt.instance).to.be.equal('Date');
     expect(sentAt).to.be.an('object');
     expect(sentAt.options.type).to.be.a('function');
@@ -231,7 +243,7 @@ describe('Message Schema', () => {
     const failedAt = Message.path('failedAt');
 
     expect(failedAt).to.exist;
-    expect(failedAt).to.be.an.instanceof(Types.Date);
+    expect(failedAt).to.be.an.instanceof(SchemaTypes.Date);
     expect(failedAt.instance).to.be.equal('Date');
     expect(failedAt).to.be.an('object');
     expect(failedAt.options.type).to.be.a('function');
@@ -244,7 +256,7 @@ describe('Message Schema', () => {
     const deliveredAt = Message.path('deliveredAt');
 
     expect(deliveredAt).to.exist;
-    expect(deliveredAt).to.be.an.instanceof(Types.Date);
+    expect(deliveredAt).to.be.an.instanceof(SchemaTypes.Date);
     expect(deliveredAt.instance).to.be.equal('Date');
     expect(deliveredAt).to.be.an('object');
     expect(deliveredAt.options.type).to.be.a('function');
@@ -257,7 +269,7 @@ describe('Message Schema', () => {
     const readAt = Message.path('readAt');
 
     expect(readAt).to.exist;
-    expect(readAt).to.be.an.instanceof(Types.Date);
+    expect(readAt).to.be.an.instanceof(SchemaTypes.Date);
     expect(readAt.instance).to.be.equal('Date');
     expect(readAt).to.be.an('object');
     expect(readAt.options.type).to.be.a('function');
@@ -270,7 +282,7 @@ describe('Message Schema', () => {
     const result = Message.path('result');
 
     expect(result).to.exist;
-    expect(result).to.be.an.instanceof(Types.Mixed);
+    expect(result).to.be.an.instanceof(SchemaTypes.Mixed);
     expect(result.instance).to.be.equal('Mixed');
     expect(result).to.be.an('object');
     expect(result.options.type).to.be.a('function');
@@ -282,7 +294,7 @@ describe('Message Schema', () => {
     const transport = Message.path('transport');
 
     expect(transport).to.exist;
-    expect(transport).to.be.an.instanceof(Types.String);
+    expect(transport).to.be.an.instanceof(SchemaTypes.String);
     expect(transport.instance).to.be.equal('String');
     expect(transport).to.be.an('object');
     expect(transport.options.type).to.be.a('function');
@@ -297,7 +309,7 @@ describe('Message Schema', () => {
     const priority = Message.path('priority');
 
     expect(priority).to.exist;
-    expect(priority).to.be.an.instanceof(Types.String);
+    expect(priority).to.be.an.instanceof(SchemaTypes.String);
     expect(priority.instance).to.be.equal('String');
     expect(priority).to.be.an('object');
     expect(priority.options.type).to.be.a('function');
@@ -315,7 +327,7 @@ describe('Message Schema', () => {
     const hash = Message.path('hash');
 
     expect(hash).to.exist;
-    expect(hash).to.be.an.instanceof(Types.String);
+    expect(hash).to.be.an.instanceof(SchemaTypes.String);
     expect(hash.instance).to.be.equal('String');
     expect(hash).to.be.an('object');
     expect(hash.options.type).to.be.a('function');
@@ -331,7 +343,7 @@ describe('Message Schema', () => {
     const tags = Message.path('tags');
 
     expect(tags).to.exist;
-    expect(tags).to.be.an.instanceof(Types.Array);
+    expect(tags).to.be.an.instanceof(SchemaTypes.Array);
     expect(tags.instance).to.be.equal('Array');
     expect(tags).to.be.an('object');
     expect(tags.options.type[0]).to.be.a('function');
@@ -344,7 +356,7 @@ describe('Message Schema', () => {
     const options = Message.path('options');
 
     expect(options).to.exist;
-    expect(options).to.be.an.instanceof(Types.Mixed);
+    expect(options).to.be.an.instanceof(SchemaTypes.Mixed);
     expect(options.instance).to.be.equal('Mixed');
     expect(options).to.be.an('object');
     expect(options.options.type).to.be.a('function');
