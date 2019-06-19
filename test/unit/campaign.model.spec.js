5faeab32610c6ad1bbf511e66bc3f4bda90379b1
lykmapipo <lallyelias87@gmail.com>
2019-06-19 17:10:03 +0300

    test: add campaign validations specs

---
+++ test/unit/campaign.model.spec.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/* dependencies */
+const path = require('path');
+const { expect } = require('@lykmapipo/mongoose-test-helpers');
+const { Campaign } = require(path.join(__dirname, '..', '..'));
+
+describe('Campaign Validation', () => {
+  it('should validate all fields', done => {
+    const campaign = Campaign.fake();
+    campaign.validate(error => {
+      expect(error).to.not.exist;
+      done();
+    });
+  });
+
+  it('should work with well known criteria format', done => {
+    const campaign = Campaign.fake();
+    campaign.criteria = { agencies: { $in: ['5d0a3ac36aeb7f1cff91c00f'] } };
+    campaign.validate(error => {
+      expect(error).to.not.exist;
+      done();
+    });
+  });
+
+  it('should work with well known criteria format', done => {
+    const campaign = Campaign.fake();
+    campaign.criteria = { agencies: { $in: undefined } };
+    campaign.validate(error => {
+      expect(error).to.not.exist;
+      done();
+    });
+  });
+
+  it('should work with well known criteria format', done => {
+    const campaign = Campaign.fake();
+    campaign.criteria = {
+      _id: { $in: ['5cffb7692e65f7001a2d5b02',
+          '5cffb7692e65f7001a2d5b1d'
+        ] },
+      group: { $in: [] },
+      role: { $in: [] },
+      location: { $in: [] },
+    };
+    campaign.validate(error => {
+      expect(error).to.not.exist;
+      done();
+    });
+  });
+});
