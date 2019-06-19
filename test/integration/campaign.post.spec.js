5318c5a5ca73837d42599a43afbbdb237a96c0b0
lykmapipo <lallyelias87@gmail.com>
2019-06-19 18:32:10 +0300

    fix: allow service criteria with mongodb keywords

--- test/integration/campaign.post.spec.js
+++ test/integration/campaign.post.spec.js
@@ -18,6 +18,22 @@ describe('Campaign Static Post', () => {
     });
   });
 
+  it('should be able to post with criteria', done => {
+    const campaign = Campaign.fake();
+    campaign.criteria = {
+      _id: { $in: ['5cffb7692e65f7001a2d5b02', '5cffb7692e65f7001a2d5b1d'] },
+      group: { $in: [] },
+      role: { $in: [] },
+      location: { $in: [] },
+    };
+    Campaign.post(campaign, (error, created) => {
+      expect(error).to.not.exist;
+      expect(created).to.exist;
+      expect(created._id).to.eql(campaign._id);
+      done(error, created);
+    });
+  });
+
   after(done => {
     Campaign.deleteMany(done);
   });
