2109d4f64e6cdfe0d0c14539f51d121fede5fdcc
lykmapipo <lallyelias87@gmail.com>
2020-02-26 01:08:18 +0300

    test(smssync): add http api spec definition

---
+++ test/integration/smssync.http.spec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { clear: clearHttp, mount } = require('@lykmapipo/express-test-helpers');
+const { clear: clearDatabase } = require('@lykmapipo/mongoose-test-helpers');
+const { smssyncRouter } = require('../..');
+
+describe('SMSSync Http API', () => {
+	before(() => clearHttp());
+	before(done => clearDatabase(done));
+	before(() => {
+		mount(smssyncRouter);
+	});
+
+	it('should receive sync sms from a device', done => {
+		done();
+	});
+
+	it('should receive queued sms to be sent by a device', done => {
+		done();
+	});
+
+	it('should receive delivery reports sent by a device', done => {
+		done();
+	});
+
+	it('should return sms to be sent by device', done => {
+		done();
+	});
+
+	it('should return sms waiting delivery report to a device', done => {
+		done();
+	});
+
+	it('should reject request with no secret key sent by device', done => {
+		done();
+	});
+
+	after(() => clearHttp());
+	after(done => clearDatabase(done));
+});
